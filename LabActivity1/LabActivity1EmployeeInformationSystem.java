abd2fb8eae1ca9e76f971a46414156d4d8e6dc22
Juzthin Constantino <juzthin.constantino@cvsu.edu.ph>
2025-03-05 13:48:52 +0800

    Add files via upload

---
+++ LabActivity1/LabActivity1EmployeeInformationSystem.java
@@ -0,0 +1,45 @@
+package LabActivity1;
+import java.util.Scanner;
+ 
+public class LabActivity1EmployeeInformationSystem {
+
+    public static void main(String[] args) {
+        
+  Scanner scan = new Scanner(System.in);
+  String FirstName;
+  String LastName;
+  int Age;
+  float NumberOfHoursWorked;
+  float HourlyWage;
+  
+//  For Getting input and Outputting Firstname
+  System.out.print("Enter your First name    : ");
+  FirstName = scan.nextLine();
+ 
+  
+  //  For Getting input and Outputting Lastname
+  System.out.print("Enter your Last name     : ");
+  LastName = scan.nextLine();
+   
+ //  For Getting input and Outputting Age
+  System.out.print("Enter your Age : ");
+  Age = scan.nextInt();
+
+//  For getting hours work input and output 
+  System.out.print("Enter your hours worked  : ");
+  NumberOfHoursWorked = scan.nextFloat();
+  
+//  For getting input for hourly wage
+  System.out.print("Enter your Hourly Wage  : ");
+  HourlyWage = scan.nextFloat();
+  
+//  For Outputting Personal Information and computing Daily Salary
+  System.out.println(" ");
+  System.out.println("Personal Information");
+  System.out.println("--------------------");
+  System.out.println("Full name    : " + FirstName + " "+ LastName);
+  System.out.println("Age          : " + Age + " Years Old");
+  System.out.println("Daily Salary : " + NumberOfHoursWorked * HourlyWage );
+  
+}
+}
